4082d0b6ac70257d993309ba0a87685aea968831
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-14 20:05:55 -0300

    Main planteado

--- guia_2/src/Main.java
+++ guia_2/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //////////////////////      Ejercicio 1 Libro       /////////////////////
+        //////////////////////      Ejercicio 2 Venta       /////////////////////
+        //////////////////////      Ejercicio 3 Cuenta Bancaria       /////////////////////
+
+
     }
 }
